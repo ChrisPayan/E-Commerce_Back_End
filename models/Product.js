a8bbb10132cc90487cf680dbe4ac6c2a510fdc6d
Christian Payan <payanstocks@gmail.com>
2021-04-25 11:36:20 -0700

    Added mdoel for tag

--- models/Product.js
+++ models/Product.js
@@ -19,6 +19,28 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      }
+    }
   },
   {
     sequelize,
