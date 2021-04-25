53d8f70474e5b3869f2bb6b929844d88a95808c6
Christian Payan <payanstocks@gmail.com>
2021-04-25 11:28:04 -0700

    Added model for product

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,16 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
