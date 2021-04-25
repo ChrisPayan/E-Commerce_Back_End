f15280b6da2bba22e221b0edf5aa7362016de91d
Christian Payan <payanstocks@gmail.com>
2021-04-25 11:47:45 -0700

    Added all assignments for one to one and one to many in Index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    as: 'tags',
+  },
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    as: 'tags',
+  },
+});
+// Export
 module.exports = {
   Product,
   Category,
